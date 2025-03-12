9f6d6089bca154bf1db397e867ab3786f2f49b82
Stan Ke <156306548@qq.com>
2025-03-12 23:38:11 +0800

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <>
+    <div className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">Hello, Vite + TailwindCSS!</h1>
+    </div>
+  );
+}
